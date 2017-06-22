5a2222ad512591d566de97d5f99bb1e315968669
Mathias Neerup <manee12@student.sdu.dk>
2017-06-22 23:46:19 +0200

    Using mDNS app from LWIP2.2

--- software/main/include/mDNSServer.h
+++ software/main/include/mDNSServer.h
@@ -11,7 +11,5 @@ class mDNSServer
     private:
         static void mdns_monitor(void *PvParameters);
         static mDNSServer *instance;
-        static mdns_server_t *server_sta;
-        static mdns_server_t *server_ap;
         mDNSServer();
 };
--- software/main/mDNSServer.cpp
+++ software/main/mDNSServer.cpp
@@ -2,16 +2,7 @@
 #include "esp_log.h"
 #include "WifiHandler.h"
 #include <cstring>
-static const char *TAG = "mdns";
-
-static const char * arduTxtData[3] = {
-    "board_rev=esp32_0.1",
-    "sockets=3",
-    "api=v1",
-};
-
-mdns_server_t *mDNSServer::server_sta = nullptr;
-mdns_server_t *mDNSServer::server_ap = nullptr;
+static const char *TAG = "mDNS";
 
 mDNSServer *mDNSServer::instance = nullptr;
 
@@ -30,49 +21,62 @@ mDNSServer::mDNSServer()
 
 };
 
-
-void advertise_mdns_records(mdns_server_t * iface){
-      ESP_ERROR_CHECK( mdns_set_hostname(iface, "powersocket") );
-      ESP_ERROR_CHECK( mdns_set_instance(iface, "A Powersocket") );
-
-      ESP_ERROR_CHECK( mdns_service_add(iface, "_powersocket_http", "_tcp", 80) );
-      ESP_ERROR_CHECK( mdns_service_txt_set(iface, "_powersocket_http", "_tcp", 3, arduTxtData) );
-      ESP_ERROR_CHECK( mdns_service_add(iface, "_powersocket_https", "_tcp", 443) );
-      ESP_ERROR_CHECK( mdns_service_txt_set(iface, "_powersocket_https", "_tcp", 3, arduTxtData) );
+static void srv_http(struct mdns_service *service, void *txt_userdata)
+{
+   err_t res = mdns_resp_add_service_txtitem(service, "ehm..", 5);
+   LWIP_ERROR("mdns add service txt failed\n", (res == ERR_OK), return);
+   LWIP_UNUSED_ARG(txt_userdata);
 }
-
 
 void mDNSServer::mdns_monitor(__attribute__((unused)) void *PvParameters)
 {
     // Housekeeping variables
     bool is_connected_sta = false;
     bool is_connected_ap = false;
-    esp_err_t err;
+    bool mdns_configured = false;
+    esp_err_t err = ERR_OK;
 
     while(true){
-      if(!is_connected_sta && (xEventGroupGetBits( WifiHandler::wifi_event_group ) & WifiHandler::AP_CONNECTED_BIT)){
-        is_connected_sta = true;
+      if(!is_connected_ap && (xEventGroupGetBits( WifiHandler::wifi_event_group ) & WifiHandler::AP_CONNECTED_BIT)){
+        is_connected_ap = true;
 
-        err = mdns_init(TCPIP_ADAPTER_IF_AP, &mDNSServer::server_ap);
+        if(!mdns_configured ){
+          mdns_configured = true;
+          ESP_LOGI(TAG, "mdns_init run");
+          mdns_resp_init();
+        }
+
+        struct netif *iface = esp_netif[TCPIP_ADAPTER_IF_AP];
+        char  hostname[] = "powersocket";
+        err = mdns_resp_add_netif(iface, hostname, 255);
+        mdns_resp_add_service(iface,"powersocket","_http", DNSSD_PROTO_TCP, 80, 255, srv_http, NULL);
         if (err) {
           ESP_LOGE(TAG, "Failed starting mDNS AP: %d", err);
         } else {
           ESP_LOGI(TAG, "Sucessfully started mDNS AP !");
         }
-        advertise_mdns_records(mDNSServer::server_ap);
       }
 
-      if(!is_connected_ap && (xEventGroupGetBits( WifiHandler::wifi_event_group ) & WifiHandler::CONNECTED_BIT)){
-        is_connected_ap = true;
+      if(!is_connected_sta && (xEventGroupGetBits( WifiHandler::wifi_event_group ) & WifiHandler::CONNECTED_BIT)){
+        is_connected_sta = true;
 
-        err = mdns_init(TCPIP_ADAPTER_IF_STA, &mDNSServer::server_sta);
+        if(!mdns_configured ){
+          mdns_configured = true;
+          ESP_LOGI(TAG, "mdns_init run");
+          mdns_resp_init();
+        }
+
+        struct netif *iface = esp_netif[TCPIP_ADAPTER_IF_STA];
+        char  hostname[] = "powersocket";
+        err = mdns_resp_add_netif(iface, hostname, 255);
+        mdns_resp_add_service(iface,"powersocket","_http", DNSSD_PROTO_TCP, 80, 255, srv_http, NULL);
         if (err) {
           ESP_LOGE(TAG, "Failed starting mDNS STA: %d", err);
         } else {
           ESP_LOGI(TAG, "Sucessfully started mDNS STA !");
         }
-        advertise_mdns_records(mDNSServer::server_sta);
       }
+
       vTaskDelay(10000 / portTICK_PERIOD_MS); //every 5 seconds
     }
-};
+}
